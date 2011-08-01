7ab4219c2413146b9416815daf56feb4a716656a
AIM <AIM@218.147.248.111>
2011-08-01 18:02:37 +0900

    12412412412412412412

--- src/Branch.java
+++ src/Branch.java
@@ -7,16 +7,16 @@ public class Branch {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-//		System.out.println("1");
-//		System.out.println("2");
-//		System.out.println("3");
-//		
-//		System.out.println("°¡");
-//		System.out.println("°¼");
-//		System.out.println("°Å");
-//		System.out.println("°Ü");
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
 		
-		//System.out.println("r");
+		System.out.println("°¡");
+		System.out.println("°¼");
+		System.out.println("°Å");
+		System.out.println("°Ü");
+		
+		System.out.println("r");
 		
 	
 		
